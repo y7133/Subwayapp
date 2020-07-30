c44739adb9d0c1199dc607d9d2f82af0fb768d16
y7133@naver.com <y7133@naver.com>
2020-07-30 18:47:30 +0900

    UI tool

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import org.w3c.dom.Document;
+import java.util.ArrayList;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    // 4개의 메뉴에 들어갈 Fragment들
+    private Menu1Frag menu1Frag = new Menu1Frag();
+    private Menu2Frag menu2Frag = new Menu2Frag();
+    private Menu3Frag menu3Frag = new Menu3Frag();
+    private Menu4Frag menu4Frag = new Menu4Frag();
+
+    //for Gps
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+
+    public static int TO_GRID = 0;
+
+    Document doc = null;
+
+
+    ArrayList<String> wtstate = new ArrayList<>();
+    ArrayList<String> tempor = new ArrayList<>();
+    ArrayList<Integer> time = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        // 프래그먼트 초기 추가
+        // 하지만 해당 프래그먼트 이동시 초기 데이터를 가져오기로 해놨기 때문에 필요없어보임
+        //transaction.attach(menu1FragGrowth);
+        //transaction.attach(menu2FragStore);
+        //transaction.attach(menu3FragWeather);
+        //transaction.attach(menu4FragConfig);
+        // 첫 화면 추가
+        transaction.replace(R.id.frame_layout, menu1Frag).commitAllowingStateLoss();
+
+        // bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.navigation_menu1: {
+                        transaction.replace(R.id.frame_layout, menu1Frag).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.navigation_menu2: {
+                        transaction.replace(R.id.frame_layout, menu2Frag).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.navigation_menu3: {
+                        transaction.replace(R.id.frame_layout, menu3Frag).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.navigation_menu4: {
+                        transaction.replace(R.id.frame_layout, menu4Frag).commitAllowingStateLoss();
+                        break;
+                    }
+                }
+
+                return true;
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            isAccessFineLocation = true;
+
+        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            isAccessCoarseLocation = true;
+        }
+
+        if (isAccessFineLocation && isAccessCoarseLocation) {
+            isPermission = true;
+        }
+    }
+
+    // 위치 권한 요청
+    private void callPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
+        } else {
+            isPermission = true;
+        }
+    }
+
+}
---
+++ MyApplication2/app/src/main/java/com/example/myapplication/Menu3Frag.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+import androidx.fragment.app.Fragment;
+
+public class Menu3Frag extends Fragment {
+}
